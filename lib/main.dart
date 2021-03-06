27ea863a0835f26436c6b17f02d14989ad042d91
jiho-choo <asuraho@gmail.com>
2021-03-07 03:44:43 +0900

    울라랄

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:youtube_clone_app/src/app.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Youtube clone App",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: "/",
+      getPages: [GetPage(name: "/", page: () => App())],
+    );
+  }
+}
